b7d9623cbdbf7f35da6fa4348d0c0979c276630e
Ryan O <46252299+oconnoob@users.noreply.github.com>
2022-06-01 22:03:05 -0400

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+echo "Upgrading packages"
+yes | sudo apt update
+yes | sudo apt upgrade
+
+echo "Installing ffmpeg"
+yes | sudo apt install ffmpeg
+
+echo "Installing sox"
+yes | sudo apt install sox
+
+echo "Installing cmake"
+yes | sudo apt-get install cmake
+
+echo "Installing pyjopentalk"
+yes | sudo apt-get install pyjopentalk==0.1.5
+
+echo "Installing parallel_wavegan"
+yes | sudo apt-get install parallel_wavegan==0.5.3
+
+echo "Cloning project folder ESPNet"
+git clone https://github.com/AssemblyAI-Examples/intro-to-espnet.git
+cd intro-to-espnet
+
+echo "Installing pip"
+yes | apt install python3-pip
+
+echo "Installing virtualenv"
+yes | apt install virtualenv
+
+
+
